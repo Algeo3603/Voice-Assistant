642acf97667fca99294079e99dab84d6586ddb20
Algeo3603 <alangeorge3603@gmail.com>
2024-05-16 20:33:27 +0530

    deployment

--- base/modules/llm.py
+++ base/modules/llm.py
@@ -2,8 +2,10 @@ import os
 from dotenv import load_dotenv
 from groq import Groq
 
-load_dotenv()
-GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+# load_dotenv()
+# GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+
+GROQ_API_KEY = os.environ['GROQ_API_KEY']
 
 # client = Groq(
 #     api_key=GROQ_API_KEY,
--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'azure_project.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'azure_project.settings'
+settings_module = 'mysite.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'mysite.settings'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
