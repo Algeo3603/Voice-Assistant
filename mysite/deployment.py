f14632624e95f202a8ae3d7f588da4d11082c4ff
Algeo3603 <alangeorge3603@gmail.com>
2024-05-17 09:49:29 +0530

    final deployment

--- mysite/deployment.py
+++ mysite/deployment.py
@@ -27,7 +27,7 @@ conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backend.postgresql',
         'NAME': conn_str_params['dbname'],
         'HOST': conn_str_params['host'],
         'USER': conn_str_params['user'],
